4740cb5cb2f30de4158f39fbabe91bab9aae3852
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2024-01-02 21:10:29 +0530

    destructuring array

--- arrays.js
+++ arrays.js
@@ -46,3 +46,31 @@ fruits.indexOf("orange");
 
 //includes(element) -> returns boolean result whether element is present or not
 fruits.includes("mango");
+
+/***********************************************************************************************
+ * Destructuring Arrays
+ * 
+ *  -> destructuring means unpacking the values of array
+ *  -> original array is not changed, we just store the values inside some variable
+ ***********************************************************************************************/
+
+const arr = [1,2,3,4];
+let [x,y,z] = arr;
+//here the assignment follows the index of array 
+//output: x = 1, y=2, z=3
+// 4 will not be assigned to any variable
+// if we have to leave any element, just give a empty comma
+console.log(x,y,z);
+
+// swapping the values 
+[y,x] = [x,y]; 
+console.log(x,y);
+
+//nested destructuring
+const nested = [2,4,[5,6]];
+const [a,,[b,c]] = nested;
+console.log(a,b,c);
+
+//assigining default values while destructuring
+const [s=1,t=1,u=1] = [2,3];
+console.log(s,t,u);
