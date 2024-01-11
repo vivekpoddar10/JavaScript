9e1708f5dff7cf8e0cf4b87e71c60c65cdd65bd5
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2024-01-11 22:20:23 +0530

    learning oops

--- oop/es6Classes.js
+++ oop/es6Classes.js
@@ -1,23 +1,58 @@
-'use strict';
+"use strict";
 //class expression
 // const Person = class{
 
 // };
 //class declaration
-class PersonCl{
-    constructor(name, birthYear) {
-        this.name = name;
-        this.birthYear = birthYear;
-    }
-    //methods will be added to the PersonCl.prototype
-    calcAge() {
-        console.log(2024 - this.birthYear);
-    }
+class PersonCl {
+  constructor(name, birthYear) {
+    this.name = name;
+    this.birthYear = birthYear;
+  }
+  //methods will be added to the PersonCl.prototype
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
 }
 
-const Vivek = new PersonCl('vivek', 2002);
+const Vivek = new PersonCl("vivek", 2002);
 console.log(Vivek);
 
 //classes are not hoisted
 //classes are first-class citizens, i.e. can be passes as an argument
 //classes are executed in strict mode
+
+//setter and getter methods
+class ClassWithGetSet {
+  msg = "hello world";
+  get msg() {
+    return this.msg;
+  }
+  set msg(x) {
+    this.msg = `hello ${x}`;
+  }
+}
+
+const instance = new ClassWithGetSet();
+console.log(instance.msg); // "hello world"
+
+instance.msg = "cake";
+console.log(instance.msg); // "hello cake"
+
+class Bank {
+  constructor(name, address) {
+    this._name = name;
+    this.address = address;
+  }
+  set name(newName) {
+    this._name = newName;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+const myBank = new Bank("SBI", "New Delhi");
+console.log(myBank.name);
+
+//static method
